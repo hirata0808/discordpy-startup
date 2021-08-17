c81325a6a8e3e9b635bec347e9e66e732de805a2
hirata0808 <89074838+hirata0808@users.noreply.github.com>
2021-08-17 19:25:10 +0900

    ver2

--- discordbot.py
+++ discordbot.py
@@ -14,10 +14,10 @@ async def on_ready():
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if member.guild.id == 600996774336790538:
-        txtRoom = client.get_channel(600996774336790540)
+    if member.guild.id == 531871814176210944:
+        txtRoom = client.get_channel(531871814176210948)
         if before.channel is None:
-            msg = f'**{member.name}**さんが **しーめい** に参加しましたけど...'
+            msg = f'**{member.name}**さんが **かおす** に参加しましたけど...'
             await txtRoom.send(msg)
 
 
